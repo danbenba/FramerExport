bff13a6ea66a75a81465d5ab2bbdc71e053ed905
Dany <89309539+danbenba@users.noreply.github.com>
2026-04-17 15:30:00 +0200

    feat(platforms): add platform barrel exports

---
+++ src/platforms/index.ts
@@ -0,0 +1,5 @@
+export type { PlatformHandler, PlatformType } from './types.js';
+export { framer } from './framer.js';
+export { webflow } from './webflow.js';
+export { wix } from './wix.js';
+export { detectPlatform, detectByUrl, detectByHtml, getPlatformByName } from './detect.js';
